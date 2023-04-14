88b169c2033950f16f6f026bc48b982aac6c9a92
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2023-04-14 16:12:31 -0300

    applying lint

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,27 @@
-import client from './helpers/client';
-import qrcode from 'qrcode-terminal';
-import './commands';
+import client from "./helpers/client";
+import qrcode from "qrcode-terminal";
+import "./commands";
 
-client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
-client.on('authenticated', () => {
-    console.log('Client is authenticated!');
+client.on("authenticated", () => {
+  console.log("Client is authenticated!");
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on("ready", () => {
+  console.log("Client is ready!");
 });
 
-client.on('message', async (msg) => {
-    let chat = await msg.getChat();
-    let name = await msg.getContact();
+client.on("message", async (msg) => {
+  let chat = await msg.getChat();
+  let name = await msg.getContact();
 
-    if (msg.body) {
-        if (chat.isGroup)
-            console.log(`[GROUP MSG] ${chat.name} - ${name.pushname}: ${msg.body}`);
-        else
-            console.log(`[PRIVATE MSG] ${name.pushname}: ${msg.body}`);
-    }
-})
+  if (msg.body) {
+    if (chat.isGroup) console.log(`[GROUP MSG] ${chat.name} - ${name.pushname}: ${msg.body}`);
+    else console.log(`[PRIVATE MSG] ${name.pushname}: ${msg.body}`);
+  }
+});
 
 client.initialize();
